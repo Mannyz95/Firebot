a08170f31bfa968c8a6cdf78275d6c325dcba592
Manuel Zuaznabar <manuelzuaznabar@Manuels-MBP.home>
2025-04-14 22:43:26 -0400

    switch to PyMuPDF to fix cursed PDFs

--- load_docs.py
+++ load_docs.py
@@ -1,23 +1,22 @@
-import os
-from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import PyMuPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.schema.document import Document
+import os
 
-def load_fdny_pdfs(folder_path):
-    docs = []
-    for root, _, files in os.walk(folder_path):
+def load_fdny_pdfs(directory):
+    all_chunks = []
+
+    for root, dirs, files in os.walk(directory):
         for file in files:
-            if file.endswith('.pdf'):
-                file_path = os.path.join(root, file)
-                loader = PyPDFLoader(file_path)
+            if file.endswith(".pdf"):
+                pdf_path = os.path.join(root, file)
+                loader = PyMuPDFLoader(pdf_path)
                 pdf_docs = loader.load()
-                for doc in pdf_docs:
-                    doc.metadata["source"] = file  # Track filename
-                docs.extend(pdf_docs)
 
-    splitter = RecursiveCharacterTextSplitter(
-        chunk_size=500,
-        chunk_overlap=50
-    )
-    chunks = splitter.split_documents(docs)
-    return chunks
+                text_splitter = RecursiveCharacterTextSplitter(
+                    chunk_size=1000,
+                    chunk_overlap=200
+                )
+                chunks = text_splitter.split_documents(pdf_docs)
+                all_chunks.extend(chunks)
+
+    return all_chunks
